2e54c4c8047d54337c4f435ca199d5d51fcf7407
Leighzer <bwc_nothgiel@live.com>
2020-05-26 23:00:34 -0400

    Initial commit; Added license; Added .gitignore; Update README;

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TifToPng
 Simple c# console app that converts a provided tif file to a png file.
+
+# Usage
+TifToPng path/to/file.tif
+
+# Details
+It will try to make a png at path/to/file.png . I made this very quickly to convert the files on my own rather than rely on an 3rd party service. Only converts a single file at a time.
---
+++ TifToPng/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+
+namespace TifToPng
+{
+    class Program
+    {
+        static void Main(string[] argsArray)
+        {
+            List<string> args = new List<string>(argsArray);
+
+            if(args.Count < 1)
+            {
+                Console.WriteLine("Not enough arguments supplied");
+                return;
+            }
+
+            string filePath = args[0];
+
+            bool fileExists = File.Exists(filePath);
+            if (fileExists)
+            {
+                try
+                {
+                    string newfilePath = Path.ChangeExtension(filePath, ".png");
+                    System.Drawing.Bitmap.FromFile(filePath).Save(newfilePath, System.Drawing.Imaging.ImageFormat.Png);
+                    Console.WriteLine("Success! Png created!");
+                }
+                catch
+                {
+                    Console.WriteLine("Tif to png translation failed");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
